ca40e193924c76f768d3f7ae0e30eca9eb7eacab
MZ1387 <maza1387@gmail.com>
2022-12-05 19:28:34 -0800

    update challenge #1 + update html favicon

--- src/challenges/index.js
+++ src/challenges/index.js
@@ -1,40 +1,39 @@
-const challenge1Definition = `
+const challengeOneDefinition = `
 Write a function that takes a string.
 
-Then return it in all caps with an exclamation mark at the end.
+Then return it in lowercase with a question mark.
 
-And if the string is a sentence with spaces, return a string with screaming emojis where the space used to be.   
+And if the string is a sentence, return a string with a 🤫 where the spaces used to be.
+
+Here's an example of the input you're gonna use and what you should get back:
 `;
 
-const challenge1Code = `
-function add() {
+const challengeOneCode = `
+// input: JIMMY!
+//  - output: jimmy!?
+
+// input: ARE YOU MR. BEAST!
+//  - output: are 🤫 you 🤫 mr. 🤫 beast?
+
+
+function quietQuestion() {
     // write your code here
 
 }
 
 
 
-// code should return 4
-console.log(add(1, 3));
+// function should return 'bruh 🤫 why?'
+console.log(quietQuestion('bruh why'));
 `;
 
 const challenges = [
 	{
-		title: 'Last To Take Hand Off Jet, Keeps It!',
-		src: 'https://i.ytimg.com/vi/kX3nB4PpJko/hqdefault.jpg?sqp=-oaymwEcCNACELwBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLAVlOdJKdF2cegO732Qfrxt1vrtPA',
-		challengeDefinition: challenge1Definition,
-		challengeCode: challenge1Code,
-		challengeAnswer: '🐶',
-	},
-	{
-		title: '$1 vs $1,000,000 Hotel Room!',
-		src: 'https://i.ytimg.com/vi/iogcY_4xGjo/hqdefault.jpg?sqp=-oaymwEcCNACELwBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLDURRfPNEqLUeKjZxaBGFGebgdgng',
-		challengeDefinition: ``,
-	},
-	{
-		title: 'Survive 100 Days In Circle, Win $500,000',
-		src: 'https://i.ytimg.com/vi/gHzuabZUd6c/hqdefault.jpg?sqp=-oaymwEcCNACELwBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLAabOpRQVrT4V_dlzRFGhYROKLwzQ',
-		challengeDefinition: ``,
+		title: '100 Kids Vs 100 Adults For $500,000',
+		src: 'https://i.ytimg.com/vi/0CTp1a-aCUM/hqdefault.jpg?sqp=-oaymwEcCNACELwBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLCDGwO95PoEV4oKl8xxN8Nul7hGHA',
+		challengeDefinition: challengeOneDefinition,
+		challengeCode: challengeOneCode,
+		challengeAnswer: '',
 	},
 ];
 
